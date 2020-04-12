e023798361a945c023d01499ac719d76caf0eb36
Lenqth <nullptr00@gmail.com>
2020-04-12 12:44:12 +0900

    バイナリフォーマット対応、クリックでリセット

--- src/js/svg_util.js
+++ src/js/svg_util.js
@@ -5,6 +5,20 @@ export default class SVG {
             .replace(/\{path\}/g, str)
             .replace(/\{fill_color\}/g, fill_color);
     }
+
+    static calculateSVGPath(multiPoly) {
+        let tokens = [];
+        for (const poly of multiPoly) {
+            for (const cycle of poly) {
+                let subtokens = []  
+                for (const [x,y] of cycle) {
+                    subtokens.push(x+','+y)
+                }
+                tokens.push("M "+subtokens.join(" L ")+" z")
+            }
+        }
+        return tokens.join(" ");
+    }
 }
 SVG._template =
 `<svg width="{width}" height="{height}" viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">
