0765569ddab0f3a716a0814900a599729632c95c
Trirachi <writeyourusernamehere@gmail.com>
2020-04-13 22:36:35 +0900

    やっつけタイトルスクリーン
    Co-authored-by: hyparbor@gmail.com <hyparbor@gmail.com>

--- src/js/game.js
+++ src/js/game.js
@@ -1,7 +1,7 @@
 import * as PIXI from 'pixi.js'
 import CityManager from './city_manager.js'
 
-import SceneTest from './scene/scene_test.js';
+import {SceneTest,SceneTitle} from './scene/'
 
 export default class Game {
     constructor() {
@@ -9,12 +9,23 @@ export default class Game {
         this.loader = new PIXI.Loader();
         document.body.appendChild(this.app.view);
 
+        this.loader.add([
+            { name: 'bunny', url: './img/bunny.png' },
+            { name: 'logo', url: './img/latenaughts_logo.png' },
+        ]);
+
         this.scene = null;
 
         this.cityManager = new CityManager();
-        var loaded = this.cityManager.load('./data/cities.dat');
-        loaded.then(() => {
-            this.scene = new SceneTest(this);
+        var citymanager_loaded = this.cityManager.load('./data/cities.dat');
+        var pixi_loaded = new Promise( (resolve, reject) => {
+            this.loader.load( (loader, resources) => {
+                resolve();
+            } )
+        });
+
+        Promise.all([citymanager_loaded, pixi_loaded]).then(() => {
+            this.scene = new SceneTitle(this);
             this.app.ticker.add(delta => this.onTick(delta));
         });
     
---
+++ src/js/scene/index.js
@@ -0,0 +1,3 @@
+
+export {default as SceneTest} from './scene_test.js';
+export {default as SceneTitle} from './scene_title.js';
